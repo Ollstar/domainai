6acc93336a7a84b1d1b1b4d9de1acb3b2bd1f045
Oliver Andrews <oltandrews@gmail.com>
2023-02-06 09:36:17 -0800

    logo change

--- domainai/components/NestedList.jsx
+++ domainai/components/NestedList.jsx
@@ -84,102 +84,102 @@ export default function NestedList({ onSubmit, setMessageInput, handleDrawerClos
         {
           entryPoint: "In-store dining",
           questions: [
-          "What menu items are available for in-store dining?",
-          "Is there a wait time for seating?",
-          "Do you have outdoor seating available?",
-          "Do you offer table service?",
-          "Do you offer a kid's menu?"
+            "What menu items are available for in-store dining?",
+            "Is there a wait time for seating?",
+            "Do you have outdoor seating available?",
+            "Do you offer table service?",
+            "Do you offer a kid's menu?"
           ]
-          },
-          {
+        },
+        {
           entryPoint: "Outdoor dining",
           questions: [
-          "What menu items are available for outdoor dining?",
-          "Is there a wait time for outdoor seating?",
-          "Do you have heaters or umbrellas for outdoor seating?",
-          "Do you offer table service for outdoor dining?",
-          "Do you offer a kid's menu for outdoor dining?"
+            "What menu items are available for outdoor dining?",
+            "Is there a wait time for outdoor seating?",
+            "Do you have heaters or umbrellas for outdoor seating?",
+            "Do you offer table service for outdoor dining?",
+            "Do you offer a kid's menu for outdoor dining?"
           ]
-          },
-          {
+        },
+        {
           entryPoint: "Takeout",
           questions: [
-          "What menu items are available for takeout?",
-          "Is there a wait time for takeout orders?",
-          "Can I place a custom takeout order?",
-          "Do you offer any discounts or promotions for takeout orders?"
+            "What menu items are available for takeout?",
+            "Is there a wait time for takeout orders?",
+            "Can I place a custom takeout order?",
+            "Do you offer any discounts or promotions for takeout orders?"
           ]
-          }
-          ]
-          }
-          ];
-          
-          const handleClick = (entryPoint) => {
-          setOpen({
-          ...open,
-          [entryPoint]: !open[entryPoint]
-          });
-          };
-          
-          const handleQuestionClick = (entryPoints, question, e) => {
-            setMessageInput(`${entryPoints} - ${question}`);
-            setTimeout(() => {
-              onSubmit(e, `${entryPoints} - ${question}`);
-            }, 500);
-            setOpen({});
-            handleDrawerClose();
-          };
-          
-          return (
-          <List
-          component="nav"
-          aria-labelledby="nested-list-subheader"
-          subheader={
-          <ListSubheader sx={{fontFamily:"poppins"}} component="div" id="nested-list-subheader">
+        }
+      ]
+    }
+  ];
+
+  const handleClick = (entryPoint) => {
+    setOpen({
+      ...open,
+      [entryPoint]: !open[entryPoint]
+    });
+  };
+
+  const handleQuestionClick = (entryPoints, question, e) => {
+    setMessageInput(`${entryPoints} - ${question}`);
+    setTimeout(() => {
+      onSubmit(e, `${entryPoints} - ${question}`);
+    }, 500);
+    setOpen({});
+    handleDrawerClose();
+  };
+
+  return (
+    <List
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+      subheader={
+        <ListSubheader sx={{ fontFamily: "poppins" }} component="div" id="nested-list-subheader">
           Quick Questions
-          </ListSubheader>
-          }
-          >
-          {data.map((group, groupIndex) => (
-          <React.Fragment key={groupIndex}>
+        </ListSubheader>
+      }
+    >
+      {data.map((group, groupIndex) => (
+        <React.Fragment key={groupIndex}>
           <ListItemButton button onClick={() => handleClick(group.group)}>
-          <ListItemIcon>
-          {open[group.group] ? <ExpandLess /> : <ExpandMore />}
-          </ListItemIcon>
-          <ListItemText primaryTypographyProps={{fontFamily:"poppins"}} primary={group.group} />
+            <ListItemIcon>
+              {open[group.group] ? <ExpandLess /> : <ExpandMore />}
+            </ListItemIcon>
+            <ListItemText primaryTypographyProps={{ fontFamily: "poppins" }} primary={group.group} />
           </ListItemButton>
           <Collapse in={open[group.group]} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-          {group.entries.map((entry, entryIndex) => (
-          <React.Fragment key={entryIndex}>
-          <ListItemButton button onClick={() => handleClick(entry.entryPoint)}>
-          <ListItemIcon>
-          {open[entry.entryPoint] ? <ChevronLeft /> : <ChevronRight />}
-          </ListItemIcon>
-          <ListItemText primaryTypographyProps={{fontFamily:"poppins", fontSize:12}} primary={entry.entryPoint} />
-          </ListItemButton>
-          <Collapse in={open[entry.entryPoint]} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-          {entry.questions.map((question, questionIndex) => (
-          <ListItemButton
-          key={questionIndex}
-          button
-          onClick={() => handleQuestionClick(question,entry.entryPoint)}
-          >
-          <ListItemIcon>
-          <ChatIcon />
-          </ListItemIcon>
-          <ListItemText primaryTypographyProps={{fontFamily:"poppins", fontSize:10}} primary={question} />
-          </ListItemButton>
-))}
-</List>
-</Collapse>
-</React.Fragment>
-))}
-</List>
-</Collapse>
-</React.Fragment>
-))}
-</List>
-);
+            <List component="div" disablePadding>
+              {group.entries.map((entry, entryIndex) => (
+                <React.Fragment key={entryIndex}>
+                  <ListItemButton button onClick={() => handleClick(entry.entryPoint)}>
+                    <ListItemIcon>
+                      {open[entry.entryPoint] ? <ChevronLeft /> : <ChevronRight />}
+                    </ListItemIcon>
+                    <ListItemText primaryTypographyProps={{ fontFamily: "poppins", fontSize: 12 }} primary={entry.entryPoint} />
+                  </ListItemButton>
+                  <Collapse in={open[entry.entryPoint]} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                      {entry.questions.map((question, questionIndex) => (
+                        <ListItemButton
+                          key={questionIndex}
+                          button
+                          onClick={() => handleQuestionClick(question, entry.entryPoint)}
+                        >
+                          <ListItemIcon>
+                            <ChatIcon />
+                          </ListItemIcon>
+                          <ListItemText primaryTypographyProps={{ fontFamily: "poppins", fontSize: 10 }} primary={question} />
+                        </ListItemButton>
+                      ))}
+                    </List>
+                  </Collapse>
+                </React.Fragment>
+              ))}
+            </List>
+          </Collapse>
+        </React.Fragment>
+      ))}
+    </List>
+  );
 };
