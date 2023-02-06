fef58d21d96d0daedf962870be6005fa86b7be65
Oliver Andrews <oltandrews@gmail.com>
2023-02-06 01:57:05 -0800

    font fix and logo

--- domainai/pages/_document.js
+++ domainai/pages/_document.js
@@ -1,11 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import theme from '/styles/theme.js';
+
+
+
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+      </Head>
       <body>
-        <Main />
+        <ThemeProvider theme={theme}>
+          <Main />
+        </ThemeProvider>
         <NextScript />
       </body>
     </Html>
